87220737b9fe927c322fbf4cf44a014523d3dc3c
Kumar Devendra <67205183+KrumarDevendra007@users.noreply.github.com>
2025-01-03 23:12:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,17 @@
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", getFormvalue);
+
+
 function getFormvalue() {
-    //Write your code here
+
+	const firstName = document.getElementById("fname").value;
+    const lastname = document.getElementById("lname").value;
+
+    
+	alert(`${firstName} ${lastname}`);
 
 }
+
+
